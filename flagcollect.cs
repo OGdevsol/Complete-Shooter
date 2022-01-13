ace2fded8ec5d6460b75ad9d5a4f0835d6925e4e
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2022-01-14 00:06:23 +0500

    Add files via upload

--- flagcollect.cs
+++ flagcollect.cs
@@ -6,15 +6,32 @@ using UnityEngine.UI;
 
 public class flagcollect : MonoBehaviour
 {
- 
+    public static flagcollect instance;
+   
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && PlayerPrefs.GetString("Mode")=="Flag")
         {
             
-            GameManager.Instance.flagcollectcounter += 1;
-            GameManager.Instance.flagscollectedtext.text = GameManager.Instance.flagcollectcounter.ToString();
             Destroy(gameObject);
+            GameManager.Instance.flagcollectcounter += 1;
+            PlayerPrefs.SetInt("TotalFlags", PlayerPrefs.GetInt("TotalFlags")+1);
+            GameManager.Instance.flagscollectedtext.text = GameManager.Instance.flagcollectcounter.ToString();
+            FlagCollectionFunctionality();
+        }
+    }
+
+    public void FlagCollectionFunctionality()
+    {
+        if (GameManager.Instance.flagcollectcounter==LevelsController.Instance.levelData[LevelsController.Instance.currentLevel].enemiesType.Count)
+        {
+            GameManager.Instance.GameComplete();
         }
     }
 }
