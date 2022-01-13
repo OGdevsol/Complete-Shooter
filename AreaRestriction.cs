4e4a563d1ce75147086d30baa1fd4b7922c5984b
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2022-01-14 00:01:00 +0500

    Add files via upload

---
+++ AreaRestriction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaRestriction : MonoBehaviour
+{
+    public static AreaRestriction instance; 
+    public GameObject[] FlagLvLBarriers;
+    public GameObject[] BombLvLBarriers;
+    public GameObject LeavingAreaPanel;
+    public GameObject player;
+    public Transform[] OriginalSpawnPos;
+   
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+       ActivateBarriers();
+    }
+    
+    public void ActivateBarriers()
+    {
+        if (PlayerPrefs.GetString("Mode")=="Flag" && PlayerPrefs.GetInt("FlagLevel")<=30)
+        {
+            FlagLvLBarriers[PlayerPrefs.GetInt("FlagLevel")-1].SetActive(true);
+        }
+        else if(PlayerPrefs.GetString("Mode")=="BombDiffuse" && PlayerPrefs.GetInt("BombLevel")<=30)
+        {
+            BombLvLBarriers[PlayerPrefs.GetInt("BombLevel")-1].SetActive(true);
+            
+        }
+    }
+    
+    
+}
